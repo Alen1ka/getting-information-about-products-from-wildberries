ef381cc5fc1ed60f6a404339566959371be76906
Alen1ka <alena.karpukhina.99@mail.ru>
2023-01-24 16:36:42 +0700

    добавила больше параметров в запрос

--- worker.py
+++ worker.py
@@ -15,20 +15,40 @@ def read_conf():
 
 def get_category_data(subcategory):
     if subcategory.get('shardKey') is None:
-        return None, None, None
-    return subcategory['shardKey'], subcategory['query'].split('&')[0], subcategory['query'].split('&')[1]
+        return None, None, None, None
+
+    query = subcategory['query']
+    query_split = query.split('&')
+    kind = ''
+    ext = ''
+    subject = query_split[0]
+
+    if query.find('ext') != - 1 and query.find('kind') != - 1:
+        kind = '&' + query_split[0]
+        subject = query_split[1]
+        ext = '&' + query_split[2]
+
+    elif query.find('ext') != - 1:
+        ext = '&' + query_split[1]
+        # subject = subcategory['query'].split('&')[0]
+
+    elif query.find('kind') != - 1:
+        kind = '&' + query_split[0]
+        subject = query_split[1]
+
+    return subcategory['shardKey'], kind, subject, ext
 
 
-def id_generator(dict_var, page_url_category):
+def find_the_right_subcategory(dict_var, page_url_category):
     for k, v in dict_var.items():
         if v == page_url_category:
             yield dict_var  # возвращает необходимую категорию
         elif isinstance(v, dict):  # если значение v является словарем
-            for id_val in id_generator(v, page_url_category):
+            for id_val in find_the_right_subcategory(v, page_url_category):
                 yield id_val
         elif isinstance(v, list):  # если значение v является списком
             for dict_i in v:  # прохожу по каждому элементу списка
-                for id_val in id_generator(dict_i, page_url_category):
+                for id_val in find_the_right_subcategory(dict_i, page_url_category):
                     yield id_val
 
 
@@ -57,15 +77,18 @@ def get_api(url):
         # найти нужную категорию товаров
         if category['pageUrl'] in page_url_category:
             need_category = category
-    for _ in id_generator(need_category, page_url_category[0]):
+    # найти нужную подкатегорию товаров
+    for _ in find_the_right_subcategory(need_category, page_url_category[0]):
         need_subcategory = _
     print(need_subcategory)
-    shard_key, ext, subject = get_category_data(need_subcategory)
-    get_pages(shard_key, ext, subject, page_url_category)
+    # достать необходимые для запроса данные
+    shard_key, kind, subject, ext = get_category_data(need_subcategory)
+    # сделать запрос и взять первые пять страниц
+    get_pages(shard_key, kind, subject, ext, page_url_category)
 
 
 # Получает информацию о товарах с первых 5 страниц категории через мобильное API Wildberries
-def get_pages(shard_key, ext, subject, page_url_category):
+def get_pages(shard_key, kind, subject, ext, page_url_category):
     response = requests.get("https://marketing-info.wildberries.ru/marketing-info/api/v6/info?curr=rub")
     client_params = {p.split('=')[0]: p.split('=')[1] for p in response.json()['xClientInfo'].split('&')}
     product_url = ""
@@ -77,7 +100,7 @@ def get_pages(shard_key, ext, subject, page_url_category):
             # конкретного зарегистрированного покупателя. Но допустим, этот параметр вам нужен. В примере spp=26
             product_url = f"https://catalog.wb.ru/catalog/{shard_key}/catalog?" \
                           f"appType={client_params['appType']}&curr={client_params['curr']}" \
-                          f"&dest={client_params['dest']}&emp={client_params['emp']}&{ext}&" \
+                          f"&dest={client_params['dest']}&emp={client_params['emp']}{ext}{kind}&" \
                           f"lang={client_params['lang']}&locale={client_params['locale']}&page={page_number}&" \
                           f"reg={client_params['reg']}&regions={client_params['regions']}&sort=popular&" \
                           f"spp={client_params['spp']}&{subject}&version={client_params['version']}"
@@ -142,5 +165,5 @@ def save_answer_kafka(response, page_number):
 
 if __name__ == '__main__':
     # get_api("https://www.wildberries.ru/promotions")
-    get_api("https://www.wildberries.ru/catalog/detyam/tovary-dlya-malysha/peredvizhenie/avtokresla-detskie")
-    # get_api("https://www.wildberries.ru/catalog/elektronika/razvlecheniya-i-gadzhety/igrovye-konsoli/playstation")
+    # get_api("https://www.wildberries.ru/catalog/detyam/tovary-dlya-malysha/peredvizhenie/avtokresla-detskie")
+    get_api("https://www.wildberries.ru/catalog/elektronika/razvlecheniya-i-gadzhety/igrovye-konsoli/playstation")
